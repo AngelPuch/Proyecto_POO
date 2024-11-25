c31b06e451e50254e8e02858720ee3a6dc3c7fbd
Angel Puch <angeljonathanpuchhernandez@gmail.com>
2024-11-25 13:38:41 -0600

    Modificación del panel de inventario

---
+++ src/main/java/org/project/salesystem/admin/gui/FillComboBox.java
@@ -0,0 +1,32 @@
+package org.project.salesystem.admin.gui;
+
+import org.project.salesystem.admin.dao.implementation.CategoryDAOImpl;
+import org.project.salesystem.admin.dao.implementation.SupplierDAOImpl;
+import org.project.salesystem.admin.model.Category;
+import org.project.salesystem.admin.model.Supplier;
+
+import javax.swing.*;
+import java.util.List;
+
+public class FillComboBox {
+    private static SupplierDAOImpl supplierDAO = new SupplierDAOImpl();
+    private static CategoryDAOImpl categoryDAO = new CategoryDAOImpl();
+
+
+    public static void fillComboBoxSupplier(JComboBox<Supplier> comboTypeSupplier) {
+        List<Supplier> supplierList;
+        supplierList = supplierDAO.readAll();
+        for (Supplier supplier: supplierList){
+            comboTypeSupplier.addItem(supplier);
+        }
+    }
+
+    public static void fillComboBoxCategory(JComboBox<Category> comboTypeCategory) {
+        List<Category> categoryList;
+        categoryList = categoryDAO.readAll();
+        for (Category category: categoryList){
+            comboTypeCategory.addItem(category);
+        }
+    }
+
+}
--- src/main/java/org/project/salesystem/admin/gui/InventoryPanel.java
+++ src/main/java/org/project/salesystem/admin/gui/InventoryPanel.java
@@ -1,8 +1,10 @@
 package org.project.salesystem.admin.gui;
 
 import org.project.salesystem.admin.controller.InventoryPanelController;
+import org.project.salesystem.admin.controller.ProductTableModel;
 import org.project.salesystem.admin.model.Category;
 import org.project.salesystem.admin.model.Supplier;
+import static org.project.salesystem.admin.gui.FillComboBox.*;
 
 import javax.swing.*;
 import javax.swing.table.TableColumn;
@@ -11,51 +13,50 @@ import java.awt.*;
 public class InventoryPanel extends JPanel {
     private ProductTableModel tableModel;
     private JTable table;
-    JPanel inputPanel;
+    private JPanel inputPanel;
 
     private JTextField txtName;
     private JTextField txtPrice;
     private JTextField txtStock;
-    JComboBox<Supplier> comboTypeSupplier;
-    JComboBox<Category> comboTypeCategory;
+    private JComboBox<Supplier> comboTypeSupplier;
+    private JComboBox<Category> comboTypeCategory;
     private InventoryPanelController controller;
 
     public InventoryPanel() {
         setLayout(new BorderLayout());
         init();
-
         this.controller = new InventoryPanelController(this, tableModel ,table);
-        controller.fillComboBoxSupplier();
-        controller.fillComboBoxCategory();
-
         add(inputPanel, BorderLayout.NORTH);
         add(new JScrollPane(table), BorderLayout.CENTER);
         setVisible(true);
     }
 
-    public String getTxtName() { return txtName.getText(); }
-    public String getTxtPrice() { return txtPrice.getText(); }
-    public String getTxtStock() { return txtStock.getText(); }
+    public JTextField getTxtName() { return txtName; }
+    public JTextField getTxtPrice() { return txtPrice; }
+    public JTextField getTxtStock() { return txtStock; }
     public JComboBox<Supplier> getComboTypeSupplier() { return comboTypeSupplier; }
     public JComboBox<Category> getComboTypeCategory() { return comboTypeCategory; }
 
     private void init() {
         tableModel = new ProductTableModel();
         table = new JTable(tableModel);
+
         TableColumn supplierColumn = table.getColumnModel().getColumn(3);
-        supplierColumn.setCellEditor(new DefaultCellEditor(new ProductTableModel().comboTypeSupplier));
+        supplierColumn.setCellEditor(new DefaultCellEditor(new ProductTableModel().getComboTypeSupplier()));
 
         TableColumn categoryColumn = table.getColumnModel().getColumn(4);
-        categoryColumn.setCellEditor(new DefaultCellEditor(new ProductTableModel().comboTypeCategory));
+        categoryColumn.setCellEditor(new DefaultCellEditor(new ProductTableModel().getComboTypeCategory()));
 
 
         inputPanel = new JPanel(new FlowLayout());
-        txtName = new JTextField(7);
-        txtPrice = new JTextField(7);
-        txtStock = new JTextField(7);
+        txtName = new JTextField(10);
+        txtPrice = new JTextField(10);
+        txtStock = new JTextField(10);
 
         comboTypeSupplier = new JComboBox<>();
         comboTypeCategory = new JComboBox<>();
+        fillComboBoxSupplier(comboTypeSupplier);
+        fillComboBoxCategory(comboTypeCategory);
 
         inputPanel.add(new JLabel("Nombre:"));
         inputPanel.add(txtName);
@@ -78,8 +79,4 @@ public class InventoryPanel extends JPanel {
         inputPanel.add(btnDelete);
 
     }
-
-
-
-
 }
