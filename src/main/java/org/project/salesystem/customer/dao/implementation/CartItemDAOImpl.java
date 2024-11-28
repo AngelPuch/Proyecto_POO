ca9c05bda36a44ace0872bb687418d119242dbaf
josxsg <grillo.silgom@gmail.com>
2024-11-28 00:32:32 -0600

    Actualizacion 27/11

--- src/main/java/org/project/salesystem/customer/dao/implementation/CartItemDAOImpl.java
+++ src/main/java/org/project/salesystem/customer/dao/implementation/CartItemDAOImpl.java
@@ -1,4 +1,47 @@
 package org.project.salesystem.customer.dao.implementation;
 
-public class CartItemDAOImpl {
+import org.project.salesystem.customer.dao.CartItemDAO;
+import org.project.salesystem.customer.model.CartItem;
+import org.project.salesystem.database.DatabaseConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+public class CartItemDAOImpl implements CartItemDAO {
+
+    @Override
+    public void addCartItem(CartItem cartItem) {
+        String query = "INSERT INTO cartitem values (null, ?, ?, ?, ?)";
+
+        try(Connection conn = DatabaseConnection.getInstance().getConnection();
+            PreparedStatement ps = conn.prepareStatement(query)) {
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+    @Override
+    public List<CartItem> getCartItems(int cartId) {
+        return List.of();
+    }
+
+    @Override
+    public void updateCartItem(int cartItemId, int quantity) {
+
+    }
+
+    @Override
+    public void deleteCartItem(int cartItemId) {
+
+    }
+
+    @Override
+    public void clearCart(int cartId) {
+
+    }
 }
