e93a6df2ea187fa2d28f471b1f005c832a7c419b
josxsg <grillo.silgom@gmail.com>
2024-12-06 11:03:10 -0600

    Merge 06/12

---
+++ src/main/java/org/project/salesystem/customer/gui/CartProductTableModel.java
@@ -0,0 +1,4 @@
+package org.project.salesystem.customer.gui;
+
+public class CartProductTableModel {
+}
---
+++ src/main/java/org/project/salesystem/customer/session/Session.java
@@ -0,0 +1,4 @@
+package org.project.salesystem.customer.session;
+
+public class Session {
+}
